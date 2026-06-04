1bf0a8ea43f9c31ace343e25e96d9ff1ee6ba8dd
velemon <143354515+velemon@users.noreply.github.com>
2026-06-04 23:05:39 +0200

    Add required validation to work schema fields
    Updated work schema to include required validation for fields.

--- models/work.js
+++ models/work.js
@@ -3,10 +3,24 @@ const mongoose = require("mongoose");
 
 // Definierar ett schema för "Work" med fält för företag, roll, beskrivning och antal år
 const workSchema = new mongoose.Schema({
-  company: String,
-  role: String,
-  description: String,
-  years: Number
+  company: {
+    // Fältet "company" är av typen String och krävs för att skapa en "Work" post
+    type: String,
+    // "required: true" innebär att detta fält måste fyllas i när en ny "Work" post skapas, annars kommer Mongoose att kasta ett valideringsfel
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  years: {
+    type: Number,
+    required: true
+  }
 });
 
 // Exporterar modellen så att den kan användas i andra delar av applikationen, t.ex. i routes
